ed1b2c025b122756640dda7c7dcf5ba3b4252a9d
David3Emmanuel <david3emmanuel@gmail.com>
2025-09-15 03:50:33 +0100

    Add custom player

--- chess_analysis/__main__.py
+++ chess_analysis/__main__.py
@@ -1,5 +1,5 @@
 from .auto import run_auto_game
-from .player import engine_player, random_player
+from .player import engine_player, random_player, custom_player
 
 if __name__ == "__main__":
-    run_auto_game((engine_player, random_player))
+    run_auto_game((custom_player('final_model.joblib'), random_player))
--- chess_analysis/player.py
+++ chess_analysis/player.py
@@ -1,7 +1,9 @@
+from chess_analysis.engine import CustomModelEngine
 from .analysis import Analysis
 from .analysis_steps import evaluate_board, process_eval, random_move, extract_move, human_move
 
 use_engine = Analysis() | evaluate_board | process_eval
+use_custom = lambda path: Analysis(engine=CustomModelEngine(path)) | evaluate_board | process_eval
 use_random = Analysis() | random_move
 use_human = Analysis() | human_move
 
@@ -13,3 +15,4 @@ def player(name: str, pipeline: Analysis):
 engine_player = player('Engine', use_engine)
 random_player = player('Random', use_random)
 human_player = player('Human', use_human)
+custom_player = lambda path: player('Custom', use_custom(path))
